e1fe945bd43e600605bdec5b1431416c2939ce90
markbennett1973 <Grachieb8B>
2016-10-02 15:54:26 +0100

    Integrated light level sensor with time display

--- light_sensor.py
+++ light_sensor.py
@@ -1,13 +1,12 @@
-# http://www.mogalla.net/201502/lichtsensor-tsl2561-am-raspberry
+# Based on http://www.mogalla.net/201502/lichtsensor-tsl2561-am-raspberry
 import smbus
-from time import sleep
 
-#Definieren der Variablen und Konstanten
-Busnummer = 1
-Adresse_TSL2561 = 0x39
+#Define variables and constants
+Bus_Number = 1
+TSL2561_Address = 0x39
 Start = 0x51
-Ambiente_High_Byte = 0
-Ambiente_Low_Byte = 0
+Ambient_High_Byte = 0
+Ambient_Low_Byte = 0
 IR_High_Byte = 0
 IR_Low_Byte = 0
 Ambiente = 0
@@ -15,36 +14,34 @@ IR = 0
 Ratio = 0
 Lux = 0
 
-#Instanzieren eines I2C Objektes
-i2cBus = smbus.SMBus(Busnummer)
+class LightSensor(object):
 
-#Starten des 400ms Messvorganges
-i2cBus.write_byte_data(Adresse_TSL2561, 0x80, 0x03)
-while True:
-#    print "Auslesen"
-    Ambiente_Low_Byte = i2cBus.read_byte_data(Adresse_TSL2561, 0x8c)
-    Ambiente_High_Byte = i2cBus.read_byte_data(Adresse_TSL2561, 0x8d)
-    Ambiente = (Ambiente_High_Byte*256)+Ambiente_Low_Byte
-#    print "Ambiente %d"  % Ambiente
-    IR_Low_Byte = i2cBus.read_byte_data(Adresse_TSL2561, 0x8e)
-    IR_High_Byte = i2cBus.read_byte_data(Adresse_TSL2561, 0x8f)
-    IR = (IR_High_Byte*256)+IR_Low_Byte
-#    print "IR %d" % IR
-    #Berechnen des Faktors IR/Ambiente
-    Ratio = IR / float (Ambiente)
-#    print "Ratio %f" % Ratio
-    #Berechnung lt. Datenblatt TSL2561T
-    if 0 < Ratio <= 0.50:
-        Lux = 0.0304*Ambiente-0.062*Ambiente*(Ratio**1.4)
-    elif 0.50 < Ratio <= 0.61:
-        Lux = 0.0224*Ambiente-0.031*IR
-    elif 0.61 < Ratio <= 0.80:
-        Lux = 0.0128*Ambiente-0.0153*IR
-    elif 0.80 < Ratio <= 1.3:
-        Lux = 0.00146*Ambiente-0.00112*IR
-    else:
-        Lux = 0
-    print "Lux = %f" % Lux
-#    print "Ende und Sleep 0,5s"
-    sleep (2)
+    def __init__(self):
+        #Instantiate an I2C object
+        self.i2cBus = smbus.SMBus(Bus_Number)
+
+        #Start the 400ms loop
+        self.i2cBus.write_byte_data(TSL2561_Address, 0x80, 0x03)
+
+    def get_light_level(self):
+        Ambient_Low_Byte = self.i2cBus.read_byte_data(TSL2561_Address, 0x8c)
+        Ambient_High_Byte = self.i2cBus.read_byte_data(TSL2561_Address, 0x8d)
+        Ambient = (Ambient_High_Byte*256)+Ambient_Low_Byte
+        IR_Low_Byte = self.i2cBus.read_byte_data(TSL2561_Address, 0x8e)
+        IR_High_Byte = self.i2cBus.read_byte_data(TSL2561_Address, 0x8f)
+        IR = (IR_High_Byte*256)+IR_Low_Byte
+        Ratio = IR / float (Ambient)
+        if 0 < Ratio <= 0.50:
+            Lux = 0.0304*Ambient-0.062*Ambient*(Ratio**1.4)
+        elif 0.50 < Ratio <= 0.61:
+            Lux = 0.0224*Ambient-0.031*IR
+        elif 0.61 < Ratio <= 0.80:
+            Lux = 0.0128*Ambient-0.0153*IR
+        elif 0.80 < Ratio <= 1.3:
+            Lux = 0.00146*Ambient-0.00112*IR
+        else:
+            Lux = 0
+
+        return Lux
+
 
--- show_time.py
+++ show_time.py
@@ -1,18 +1,21 @@
 import time
 import datetime
 from Adafruit_LED_Backpack import SevenSegment
+import light_sensor
 
-# Create display instance on default I2C address (0x70) and bus number.
 display = SevenSegment.SevenSegment()
+sensor = light_sensor.LightSensor()
 
 display.begin()
-
-# 0 - 15
-display.set_brightness(8)
+display.set_colon(True)
 
 while True:
-        display.set_colon(True)
-        myTime = datetime.datetime.now().strftime("%I%M").lstrip('0')
-        display.print_number_str(myTime)
-        display.write_display()
-        time.sleep(1)
+    myTime = datetime.datetime.now().strftime("%I%M").lstrip('0')
+    display.print_number_str(myTime)
+    light_level = int(sensor.get_light_level())
+    if light_level > 15:
+        light_level = 15
+
+    display.set_brightness(light_level)
+    display.write_display()
+    time.sleep(1)
