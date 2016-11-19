1209445652cb9ba27ec5560508ed99c8b40624a1
markbennett1973 <Grachieb8B>
2016-11-19 15:26:05 +0000

    Add increasing volume to alarm song

--- alarm.py
+++ alarm.py
@@ -11,6 +11,7 @@ class Alarm:
     alarmIsSounding = False
     nextAlarm = None
     api = None
+    alarmVolumeThread = None
 
     def __init__(self, api):
         """ Get the next alarm time, and start a background thread to get new alarm times
@@ -24,16 +25,23 @@ class Alarm:
         thread.daemon = True
         thread.start()
 
+        self.alarmVolumeThread = threading.Thread(target=self.__increase_alarm_volume())
+
     def sound_alarm(self):
         """ Sound an alarm
         """
         logging.debug('Alarm:sounding alarm')
         pygame.mixer.init()
-        pygame.mixer.music.load("cherub_rock.mp3")
+        pygame.mixer.music.load("alarm.mp3")
         pygame.mixer.music.play()
+
+        # Volume will be increased by the increaseVolume thread
+        pygame.mixer.music.set_volume(0)
 
         self.nextAlarm = None
         self.alarmIsSounding = True
+
+        self.alarmVolumeThread.start()
 
     def stop_alarm(self):
         """ Stop an alarm from sounding
@@ -60,4 +68,9 @@ class Alarm:
         while True:
             logging.debug('Alarm:getting next alarm event')
             self.nextAlarm = self.api.get_next_alarm_event()
-            time.sleep(60)
+            time.sleep(300)
+
+    def __increase_alarm_volume(self):
+        while self.alarmIsSounding:
+            pygame.mixer.music.set_volume(pygame.mixer.music.get_volume() + 0.1)
+            time.sleep(10)
