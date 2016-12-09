01b6d1c8631ee187ceeb343aee8f4e3b42fb44c2
markbennett1973 <Grachieb8B>
2016-12-09 08:02:07 +0000

    Reinstated hardware-dependent imports

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-#import display
+import display
 import alarm
-#import button
-#import timer
-#import time
+import button
+import timer
 import google_api
 import logging
 from flask import Flask
@@ -14,15 +13,12 @@ logging.basicConfig(format='%(asctime)s %(message)s', filename='alarm.log', leve
 """ Initialise the alarm clock objects
 """
 my_api = google_api.GoogleApi()
-#my_display = display.Display()
+my_display = display.Display()
 my_alarm = alarm.Alarm(my_api)
-#my_button = button.Button(my_alarm, my_display)
-#my_timer = timer.Timer(my_display, my_alarm, my_button)
+my_button = button.Button(my_alarm, my_display)
+my_timer = timer.Timer(my_display, my_alarm, my_button)
 
 app = Flask(__name__)
 app.register_blueprint(routes)
 
 app.run(host='0.0.0.0', port=8080)
-
-#while True:
-#    time.sleep(1)
